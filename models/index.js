609fcf2bc7edf60d689f4687c6854b209b10a630
daejo <daejo@icloud.com>
2020-10-15 23:52:16 -0700

    adds index model file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, { 
+  as: 'Products', 
+  foreignKey: 'category_id',
+  constraints: false 
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  as: 'Categories',
+  foreignKey: 'category_id',
+  constraints: false 
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  as: 'ProductTags',
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  as: 'Tags',
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
