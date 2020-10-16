0f6191d72922ec6330aa220016ddf1322976ac3e
daejo <daejo@icloud.com>
2020-10-15 23:54:44 -0700

    fixes product model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // product name query 
+    product_name: {
+      types: DataTypes.STRING,
+      allowNull: false
+    },
+    // price query 
+    price: {
+      types: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    // stock query 
+    stock: {
+      types: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    // category id query 
+    category_id: {
+      types: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
